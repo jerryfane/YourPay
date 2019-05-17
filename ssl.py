1d5d1f646bae231fa13ad3704a00de441fd88a39
jerryfane <38183696+jerryfane@users.noreply.github.com>
2019-05-17 14:11:20 +0200

    Delete ssl.py

--- ssl.py
+++
@@ -1,7 +0,0 @@
-from bottle import request, route, run, template, redirect, static_file
-from config import *
-from functions import *
-
-@route('/.well-known/acme-challenge/<filename:path>')
-def send_static(filename):
-    return static_file(filename, root='ssl/')
